9b639ad3b5c26761d54262c70fecbd3c2eabab19
Sufyan <Sufyan265@users.noreply.github.com>
2024-04-12 19:29:51 +0500

    add cors manualy in index

--- index.js
+++ index.js
@@ -14,6 +14,13 @@ app.use(cors({
   origin: 'https://inotebook-gamma-dusky.vercel.app'
 }));
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://inotebook-gamma-dusky.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.use(express.json())
 
 // "Routes" use krny k liye syntax ↓ 
